b63300f7216565be7dda79bcdbcbcf775c4b7c33
LesnickBear <Lesnickbear@gmail.com>
2018-02-08 18:08:11 +0200

    Added Circle model class Added UI class controller and Fxml form model file

---
+++ src/com/zaika/Model/Circle.java
@@ -0,0 +1,48 @@
+package com.zaika.Model;
+
+public class Circle {
+    public double getDiameter() {
+        return diameter;
+    }
+
+    private void setDiameter(double diameter) {
+        this.diameter = diameter;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    private void setSquare(double square) {
+        this.square = square;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
+    private void setPerimeter(double perimeter) {
+        this.perimeter = perimeter;
+    }
+
+    private double diameter;
+    private double square;
+    private double perimeter;
+
+    public Circle(double radius) {
+
+        setDiameter(radius * 2);
+        setPerimeter(2 * Math.PI * radius);
+        setSquare(Math.PI * Math.PI * radius);
+    }
+
+    @Override
+    public String toString() {
+        return "diameter: " + diameter +
+                "; square: " +
+                square +
+                "; perimeter: " +
+                perimeter +
+                ';';
+    }
+}
---
+++ src/com/zaika/UI/MainFormController.java
@@ -0,0 +1,31 @@
+package com.zaika.UI;
+
+import com.zaika.Model.Circle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class MainFormController {
+    public Button CreateNewRoundButton;
+    @FXML
+    TextField radiusTextLine;
+    @FXML
+    TextField InfoTextLine;
+
+    @FXML
+    public void HandleClick() {
+        try {
+            double ValueOfRadius = Double.parseDouble(this.radiusTextLine.getText());
+            Circle circle = new Circle(ValueOfRadius);
+            InfoTextLine.setText(circle.toString());
+        }
+        catch (Exception e)
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR,e.getMessage());
+            alert.show();
+        }
+
+    }
+}
